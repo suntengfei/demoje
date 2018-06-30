e52ed70d3cbf4304700ccf75527abca188d74c15
suntengfei <suntengfei@he.chinamobile.com>
2018-06-30 16:25:18 +0800

    创建连接mysql connector

--- src/JVMDemo.java
+++ src/JVMDemo.java
@@ -1,6 +1,27 @@
 import javax.sound.midi.Soundbank;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class JVMDemo {
+
+    public Connection getConnection(){
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","123123");
+            //System.out.println(conn.toString());
+            return conn;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     private String name;
 
     private static int num;
@@ -17,6 +38,7 @@ public class JVMDemo {
         System.out.println(name);
     }
     public static void main(String[] args){
-        System.out.println("ok");
+        new JVMDemo().getConnection();
+        //System.out.println("ok");
     }
 }
